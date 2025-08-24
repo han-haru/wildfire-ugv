2cc1117038ee3a7e6701416a43e31f953decd447
han_haru <164311866+han-haru@users.noreply.github.com>
2025-08-25 00:57:44 +0900

    Update controllers.launch.py

--- src/fire_bringup/launch/controllers.launch.py
+++ src/fire_bringup/launch/controllers.launch.py
@@ -42,18 +42,6 @@ def generate_launch_description():
         output='screen'
     )])
 
-    #velocity_controllers/JointGroupVelocityController
-    #★ 중요: diff_drive는 타입까지 강제
-    # diff = TimerAction(period=1.5, actions=[Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     arguments=[
-    #         'diff_drive_controller',
-    #         #'--controller-type', 'diff_drive_controller/DiffDriveController',
-    #         '--param-file', ros2_control_params,
-    #     ] + cm_arg + cm_timeout,
-    #     output='screen'
-    # )])
 
     diff = TimerAction(period=1.5, actions=[Node(
         package='controller_manager',
@@ -69,39 +57,3 @@ def generate_launch_description():
 
     return LaunchDescription([jsb, arm, blade, diff])
 
-    # bringup_pkg = get_package_share_directory('fire_bringup')
-    # ros2_control_params = os.path.join(bringup_pkg, 'config', 'controller.yaml')
-
-    # # 공통 옵션들
-    # cm_node = '/controller_manager'
-    # cm_arg = ['--controller-manager', cm_node]
-    # cm_timeout = ['--controller-manager-timeout', '60']  # 필요시 60초로 넉넉히
-
-
-    # jsb = Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
-    #     output='screen'
-    # )
-    # arm = TimerAction(period=0.5, actions=[Node(
-    #     package='controller_manager', executable='spawner',
-    #     arguments=['arm_position_controller', '--controller-manager', '/controller_manager'],
-    #     output='screen'
-    # )])
-    # blade = TimerAction(period=1.0, actions=[Node(
-    #     package='controller_manager', executable='spawner',
-    #     arguments=['blade_velocity_controller', '--controller-manager', '/controller_manager'],
-    #     output='screen'
-    # )])
-    # # 4) Diff drive controller  ← 여기서도 --param-file 꼭 전달
-    # diff = TimerAction(period=1.5, actions=[Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     arguments=[
-    #         'diff_drive_controller',
-    #         '--param-file', ros2_control_params
-    #     ] + cm_arg + cm_timeout,
-    #     output='screen'
-    # )])
-    # return LaunchDescription([jsb, arm, blade, diff])
